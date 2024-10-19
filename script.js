43af1f1ce7ce13f39e5f7becaed842cf4460ae0f
ivanvargass <165545622+IvanVargas21@users.noreply.github.com>
2024-10-19 11:05:30 +0800

    Initial commit: HTML,CSS files, images, and project section

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+const body = document.querySelector("body")
+
+function changeBackgroundColor(){
+    let color = '#fff';
+    if(body.style.backgroundColor === color){
+        body.style.backgroundColor = '#000';
+    }
+}
+  const btn = document.querySelector(".switch-bg-color");
+  btn.onclick = changeBackgroundColor;
+  */
